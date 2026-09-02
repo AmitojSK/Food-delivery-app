9e818229e010bae4ae29fb4b7826247b41b4fff4
UnstackingTech <amitojsinghkochhar@gmail.com>
2026-09-02 17:36:53 +0530

    enable Redis-backed caching for restaurant and catalogue services

--- services/food-catalogue-service/src/main/java/com/fooddelivery/foodcatalogueservice/FoodCatalogueServiceApplication.java
+++ services/food-catalogue-service/src/main/java/com/fooddelivery/foodcatalogueservice/FoodCatalogueServiceApplication.java
@@ -2,8 +2,10 @@ package com.fooddelivery.foodcatalogueservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class FoodCatalogueServiceApplication {
 
     public static void main(String[] args) {
